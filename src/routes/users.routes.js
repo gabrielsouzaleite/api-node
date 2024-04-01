fc1824175149c744fdd42dedf923986ce09b2cee
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-04-01 20:58:25 -0300

    Project finished

---
+++ src/routes/users.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+function myMiddleware(request, response, next) {
+  next();
+}
+
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
+
+module.exports = usersRoutes;
