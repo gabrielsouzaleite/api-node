e145fe74a7234a48a0f555349258867792085f0f
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-05-02 17:27:24 -0300

    Segundo bloco concluído

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,19 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
-const ensureAuthenticared = require("../middlewares/ensureAuthenticated")
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticared = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
-
-function myMiddleware(request, response, next) {
-  next();
-}
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticared, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticared, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
